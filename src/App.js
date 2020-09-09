c975054637b5a3763eccea27d640cc04fe878ef6
Jonathan Greene <jon@greenethumb.com>
2020-09-09 13:52:52 -0500

    adding in react and making the App component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Track Star</h1>
+        <div>Hello!</div>
+      </header>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
