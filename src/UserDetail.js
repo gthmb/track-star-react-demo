e68e47c7807bd0c66a3907cb5f18cca799d78b66
Jonathan Greene <jon@greenethumb.com>
2020-09-10 00:26:25 -0500

    built out UserDetail and added punch in/out actions

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { getUserDetail } from './ApiService';
+import { getUserDetail, punchUserIn, punchUserOut } from './ApiService';
 import { navigate } from '@reach/router';
+import TimeEntry from './TimeEntry';
+import StatusIndicator from './StatusIndicator';
 
 const UserDetail = ({ id }) => {
     const [user, updateUser] = useState(null);
@@ -13,7 +15,48 @@ const UserDetail = ({ id }) => {
         navigate('/');
     }
 
-    return <div className="user-detail">{user && <pre>{JSON.stringify(user, null, 4)}</pre>}</div>;
+    const handlePunchIn = () => {
+        punchUserIn(id).then(updateUser);
+    };
+
+    const handlePunchOut = () => {
+        punchUserOut(user.activeEntry).then(updateUser);
+    };
+
+    return (
+        <div className="user-detail">
+            {user && (
+                <>
+                    <div className="user-detail-header">
+                        <h1>
+                            {user.firstName} {user.lastName}
+                        </h1>
+                        <p>{user.company}</p>
+                    </div>
+                    <ul>
+                        {user['time-entries'].length > 0 ? (
+                            user['time-entries'].map((entry) => (
+                                <TimeEntry key={entry.id} {...entry} />
+                            ))
+                        ) : (
+                            <li>No Time Entries</li>
+                        )}
+                    </ul>
+                    <div className="user-actions">
+                        <StatusIndicator status={user.status} />
+                        {user.status === 'away' && (
+                            <button onClick={handlePunchIn}>Punch In</button>
+                        )}
+                        {user.status === 'active' && (
+                            <button className="light-grey" onClick={handlePunchOut}>
+                                Punch Out
+                            </button>
+                        )}
+                    </div>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default UserDetail;
